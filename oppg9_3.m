6bb68f3d88eebc597ea36f9983b95559750714e0
Erik Hide Sæternes <erik.hs.93@gmail.com>
2016-04-10 19:04:31 +0200

    Add changes to task 9.2 and 9.3

--- oppg9_3.m
+++ oppg9_3.m
@@ -6,9 +6,18 @@ g = getBackProjection(fSinogram);
 F = fft2(f);
 G = fft2(g);
 
+set(groot, 'defaultTextInterpreter', 'latex');
+set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
+set(groot, 'defaultLegendInterpreter', 'latex');
+
 fourierImageBackProjection = figure();
 subplot(1,2,1), imagesc(log(abs(F))), colorbar, axis square;
+set(gca,'xtick',[]), set(gca,'xticklabel',[])
+set(gca,'ytick',[]), set(gca,'yticklabel',[])
+
 subplot(1,2,2), imagesc(log(abs(G))), colorbar, axis square;
+set(gca,'xtick',[]), set(gca,'xticklabel',[])
+set(gca,'ytick',[]), set(gca,'yticklabel',[])
 
 saveTightFigure(fourierImageBackProjection, ...
     'figures/fourierImageBackProjection.pdf'); % saves figure
