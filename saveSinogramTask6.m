98867b51b00efe359674d43777acf4d2f51a4677
sindrehenriksen <sindrehenriksen93@gmail.com>
2016-04-13 13:26:00 +0200

    Correct save file names

--- saveSinogramTask6.m
+++ saveSinogramTask6.m
@@ -5,4 +5,4 @@ NTheta = 180;
 im = zeros(N);
 im(7:10,7:10) = 1;
 sinogram = getSinogram(im, N, NTheta);
-save('sinograms/sinogramTask6.txt sinogram -ascii');
+save('sinograms/sinogramTask6.txt', 'sinogram', '-ascii');
